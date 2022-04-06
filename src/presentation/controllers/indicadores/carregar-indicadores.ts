38b37dbe00254b6f3120f0bb8f1ce7bf7f0c8a23
Lourival Sousa <lourivalsousadev@gmail.com>
2022-04-06 11:17:08 -0300

    Camada presentation finalizada

---
+++ src/presentation/controllers/indicadores/carregar-indicadores.ts
@@ -0,0 +1,27 @@
+import { ResultadoModel } from "@/domain/models/indicadores";
+import { CarregarCasos, CarregarDadosVacinacao, CarregarEstados } from "@/domain/usecases/indicadores";
+import { ok, serverError } from "@/presentation/helpers";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+
+export class CarregarIndicadoresController implements Controller {
+  constructor(
+    private readonly carregarCasos: CarregarCasos,
+    private readonly carregarDadosVacinacao: CarregarDadosVacinacao,
+    private readonly carregarEstados: CarregarEstados
+  ) {}
+
+  async lidar(): Promise<HttpResponse> {
+    try{
+      const casosBrasil = await this.carregarCasos.carregarCasos()
+      const dadosVacinacao = await this.carregarDadosVacinacao.carregarDados()
+      const estados = await this.carregarEstados.carregarEstados()
+      const resultado: ResultadoModel = {
+        pais: {...casosBrasil,...dadosVacinacao},
+        estados: estados
+      }
+      return ok(resultado)
+    }catch(erro){
+      return serverError(erro)
+    }
+  }
+}
