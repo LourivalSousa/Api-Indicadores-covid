a20a1970c582631b34199bdd7842431513bbf500
Lourival Sousa <lourivalsousadev@gmail.com>
2022-04-07 13:02:16 -0300

    Refatoração no arquivo e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/main/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,9 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/indicadores (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/indicadores')
       .expect(200)
-      .expect('Hello World!');
   });
 });
