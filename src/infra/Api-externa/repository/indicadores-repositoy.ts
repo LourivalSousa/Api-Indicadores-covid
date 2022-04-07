fda3e587049c8433da5bfbd08a31d2f9ce2f50a9
Lourival Sousa <lourivalsousadev@gmail.com>
2022-04-07 12:59:16 -0300

    Refatoranção no repository de indicadores

--- src/infra/Api-externa/repository/indicadores-repositoy.ts
+++ src/infra/Api-externa/repository/indicadores-repositoy.ts
@@ -3,7 +3,7 @@ import { CarregarCasosRepository, CarregarDadosVacinacaoRepository, CarregarEsta
 import { CasosModel, EstadoModel, VacinacaoModel } from '@/domain/models/indicadores'
 import { CarregarEstados } from '@/domain/usecases/indicadores'
 import { URLCASOS, URLVACINACAO } from '../providers'
-import { formatarDataDiaMesAno } from '@/infra/utils'
+import { formatarDataDiaMesAno }  from '../../utils/formatar-data'
 
 export class IndicadoresRepository implements
   CarregarCasosRepository,
@@ -11,60 +11,72 @@ export class IndicadoresRepository implements
   CarregarEstadosRepository {
 
   async carregarCasos(): Promise<CarregarCasosRepository.Resultado> {
-    const {data: casos} = await axios.get(URLCASOS)
-    let casosBrasil: CasosModel
-    for(let caso in casos) {
-      if(caso === 'All') {
-        const {
-          country,
-          life_expectancy,
-          confirmed,
-          population,
-          deaths
-        } = casos[caso]
+    try{
+      const {data: casos} = await axios.get(URLCASOS)
+      let casosBrasil: CasosModel
+      for(let caso in casos) {
+        if(caso === 'All') {
+          const {
+            country,
+            life_expectancy,
+            confirmed,
+            population,
+            deaths
+          } = casos[caso]
 
-         casosBrasil = {
-         nome: country,
-         expectativa_vida: Number(life_expectancy),
-         total_casos_confirmados: confirmed,
-         numero_populacao: population,
-         total_obitos: deaths
+          casosBrasil = {
+          nome: country,
+          expectativa_vida: Number(life_expectancy),
+          total_casos_confirmados: confirmed,
+          numero_populacao: population,
+          total_obitos: deaths
+          }
         }
       }
+      return casosBrasil
+    }catch(erro){
+      throw erro
     }
-    return casosBrasil
   }
 
   async carregarDados(): Promise<CarregarDadosVacinacaoRepository.Resultado> {
-    const {data} = await axios.get(URLVACINACAO)
-    const {All: dadosVacinacao} = data
-    const {administered,people_vaccinated, people_partially_vaccinated } = dadosVacinacao
-    const dadosVacinacaoFiltrados: VacinacaoModel = {
-      total_doses_aplicadas: administered,
-      pessoas_vacinadas: people_vaccinated,
-      pessoas_parcialmente_vacinadas: people_partially_vaccinated,
+    try{
+      const {data} = await axios.get(URLVACINACAO)
+      const {All: dadosVacinacao} = data
+      const {administered,people_vaccinated, people_partially_vaccinated } = dadosVacinacao
+      const dadosVacinacaoFiltrados: VacinacaoModel = {
+        total_doses_aplicadas: administered,
+        pessoas_vacinadas: people_vaccinated,
+        pessoas_parcialmente_vacinadas: people_partially_vaccinated,
+      }
+      return dadosVacinacaoFiltrados
+    }catch(erro){
+      throw erro
     }
-    return dadosVacinacaoFiltrados
   }
 
   async carregarEstados(): Promise<CarregarEstados.Resultado> {
-    const {data: casos} = await axios.get(URLCASOS)
-    const estados: Array<EstadoModel> = []
+    try{
+      const {data: casos} = await axios.get(URLCASOS)
+      const estados: Array<EstadoModel> = []
 
-    for(let caso in casos) {
-      if(caso !=='All') {
-        const {confirmed, deaths, updated} = casos[caso]
-        const dataFormatoDiaMesAno = formatarDataDiaMesAno(updated)
-        const estado: EstadoModel = {
-          nome: caso,
-          confirmados: confirmed,
-          obitos: deaths,
-          ultima_atualizacao: dataFormatoDiaMesAno
+      for(let caso in casos) {
+        if(caso !=='All') {
+          const {confirmed, deaths, updated} = casos[caso]
+          const dataFormatoDiaMesAno = formatarDataDiaMesAno(updated)
+          const estado: EstadoModel = {
+            nome: caso,
+            confirmados: confirmed,
+            obitos: deaths,
+            ultima_atualizacao: dataFormatoDiaMesAno
+          }
+          estados.push(estado)
         }
-        estados.push(estado)
       }
+      return estados
+    }catch(erro){
+      throw erro
     }
-    return estados
   }
 }
 
